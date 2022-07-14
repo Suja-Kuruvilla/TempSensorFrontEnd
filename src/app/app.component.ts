4126a7a8fe179e8c81f3c69b2a655ce1ca1e4f18
Suja Kuruvilla <suja_abraham@yahoo.com>
2022-07-14 17:36:31 -0500

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { WebSocketService } from './webSocket/web-socket.service';
+import * as d3 from 'd3';
+
+
 
 @Component({
   selector: 'app-root',
@@ -11,12 +14,81 @@ export class AppComponent {
   title = 'angular-temp-monitoring';
   subscription: Subscription;
 
+
+  private data = [{}];
+  private svg;
+  private margin = 50;
+  private width = 750 - (this.margin * 2);
+  private height = 400 - (this.margin * 2);
+
+ private createSvg(): void {
+    this.svg = d3.select("figure#bar")
+    .append("svg")
+    .attr("width", this.width + (this.margin * 2))
+    .attr("height", this.height + (this.margin * 2))
+    .append("g")
+    .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
+}
+
+private drawBars(data: any[]): void {
+  // Create the X-axis band scale
+  const x = d3.scaleBand()
+  .range([0, this.width])
+  .domain(data.map(d => d.Room))
+  .padding(0.2);
+
+  // Draw the X-axis on the DOM
+  this.svg.append("g")
+  .attr("transform", "translate(0," + this.height + ")")
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+  .attr("transform", "translate(-10,0)rotate(-45)")
+  .style("text-anchor", "end");
+
+  // Create the Y-axis band scale
+  const y = d3.scaleLinear()
+  .domain([0, 100])
+  .range([this.height, 0]);
+
+  // Draw the Y-axis on the DOM
+  this.svg.append("g")
+  .call(d3.axisLeft(y));
+
+  // Create and fill the bars
+  this.svg.selectAll("bars")
+  .data(data)
+  .enter()
+  .append("rect")
+  .attr("x", d => x(d.Room))
+  .attr("y", d => y(d.Temp))
+  .attr("width", x.bandwidth())
+  .attr("height", (d) => this.height - y(d.Temp))
+  .attr("fill", "#d04a35");
+}
+
   constructor(private webSocketService: WebSocketService) {}
 
   ngOnInit() {
-    this.subscription = this.webSocketService.cretateObservableSocket("wss://48p78wylxg.execute-api.us-east-1.amazonaws.com/production")
-    .subscribe(
-      data=> console.log("messge from the socket")
+
+    this.subscription = this.webSocketService.cretateObservableSocket("wss://11cyf2wu2d.execute-api.us-east-1.amazonaws.com/development")
+     // "wss://48p78wylxg.execute-api.us-east-1.amazonaws.com/production")
+    .subscribe(  
+      data=> {
+        const obj = JSON.parse(data);
+        const room = obj["state"]["reported"]["location"];
+        const temp = obj["state"]["reported"]["temp"];
+        console.log("messge from the socket "+ room + " "+ temp);
+        this.data.push({"Room": room, "Temp": temp});
+      },
+      err => console.log("Error from the socket"),
+      () => console.log("observable stream is complete")
     );
+
+
+  }
+
+  onDrawClicked(){
+    this.createSvg();
+    this.drawBars(this.data);
   }
 }
--- src/app/webSocket/web-socket.service.ts
+++ src/app/webSocket/web-socket.service.ts
@@ -11,12 +11,15 @@ export class WebSocketService {
       return new Observable(
         observer => {
           this.socket.onmessage = (event)=> {
-            console.log("Receiving" + event.data);
+            console.log("WebSocketService Receiving" + event.data);
             observer.next(event.data);
           }
           this.socket.onerror = (event)=> {
-            console.log("On error");
+            console.log("WebSocketService On error");
             observer.error(event);
+          }
+          this.socket.onclose = (event)=>{
+            console.log("WebSocketService On Close");
           }
           return ()=> this.socket.close(1000, "this user disconnecte");
         }
